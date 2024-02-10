f63de8a0b2f392e1cd84cd0aae4ac29d76dc894c
tineggarcia <tineggarcia@gmail.com>
2024-02-10 23:36:18 +0000

    Edit views.py to test the secret key.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+    return HttpResponse(os.environ.get("SECRET_KEY"))
