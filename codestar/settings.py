d6dac8561623af2e0faa55eb87ce6750bb46e454
tineggarcia <tineggarcia@gmail.com>
2024-02-11 00:02:53 +0000

    Modify files for POST Models.

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,6 +92,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://127.0.0.1",
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
